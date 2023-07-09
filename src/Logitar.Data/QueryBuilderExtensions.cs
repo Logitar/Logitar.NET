e395d26b49c7da911fd5534bc3c088d5661b283e
Francis Pion <francispion@hotmail.com>
2023-07-09 00:58:58 -0400

    Created QueryBuilderExtensions.

---
+++ src/Logitar.Data/QueryBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Logitar.Data;
+
+public static class QueryBuilderExtensions
+{
+  public static IQueryBuilder SelectAll(this IQueryBuilder builder)
+  {
+    return builder.Select(ColumnId.All());
+  }
+  public static IQueryBuilder SelectAll(this IQueryBuilder builder, TableId table)
+  {
+    return builder.Select(ColumnId.All(table));
+  }
+
+  public static IQueryBuilder Where(this IQueryBuilder builder, ColumnId column, ConditionalOperator @operator)
+  {
+    return builder.Where(new OperatorCondition(column, @operator));
+  }
+  public static IQueryBuilder WhereAnd(this IQueryBuilder builder, params Condition[] conditions)
+  {
+    return builder.Where(new AndCondition(conditions));
+  }
+  public static IQueryBuilder WhereOr(this IQueryBuilder builder, params Condition[] conditions)
+  {
+    return builder.Where(new OrCondition(conditions));
+  }
+
+  public static IQueryBuilder OrderBy(this IQueryBuilder builder, ColumnId column, bool isDescending = false)
+  {
+    return builder.OrderBy(new OrderBy(column, isDescending));
+  }
+}
