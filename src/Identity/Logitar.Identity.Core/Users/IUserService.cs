0fcba402bb1c5e144bbd7d2d13f831b5204a051a
Francis Pion <francispion@hotmail.com>
2023-07-25 19:58:32 -0400

    Implemented User replacement. (#64)

--- src/Identity/Logitar.Identity.Core/Users/IUserService.cs
+++ src/Identity/Logitar.Identity.Core/Users/IUserService.cs
@@ -13,5 +13,6 @@ public interface IUserService
   Task<User?> ReadAsync(string? id = null, string? tenantId = null, string? uniqueName = null, CancellationToken cancellationToken = default);
   Task<SearchResults<User>> SearchAsync(SearchUserPayload payload, CancellationToken cancellationToken = default);
   Task<User?> SignOutAsync(string id, CancellationToken cancellationToken = default);
+  Task<User?> UpdateAsync(string id, ReplaceUserPayload payload, CancellationToken cancellationToken = default);
   Task<User?> UpdateAsync(string id, UpdateUserPayload payload, CancellationToken cancellationToken = default);
 }
