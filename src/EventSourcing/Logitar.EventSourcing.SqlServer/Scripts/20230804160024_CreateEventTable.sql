8e8265f98ef043a810de954711f35b1309262a81
Francis Pion <francispion@hotmail.com>
2023-08-04 12:22:49 -0400

    Removed __EFMigrationsHistory table from raw SQL scripts.

--- src/EventSourcing/Logitar.EventSourcing.SqlServer/Scripts/20230804160024_CreateEventTable.sql
+++ src/EventSourcing/Logitar.EventSourcing.SqlServer/Scripts/20230804160024_CreateEventTable.sql
@@ -1,13 +1,3 @@
-IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
-
 BEGIN TRANSACTION;
 GO
 
@@ -45,10 +35,6 @@ CREATE INDEX [IX_Events_OccurredOn] ON [Events] ([OccurredOn]);
 GO
 
 CREATE INDEX [IX_Events_Version] ON [Events] ([Version]);
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230804160024_CreateEventTable', N'7.0.9');
 GO
 
 COMMIT;
