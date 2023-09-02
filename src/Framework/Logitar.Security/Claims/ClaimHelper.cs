688d6af5d241bca3caa43b0911b44847b6fbd7a8
Francis Pion <francispion@hotmail.com>
2023-09-01 23:55:24 -0400

    Implemented a ClaimHelper.

---
+++ src/Framework/Logitar.Security/Claims/ClaimHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace Logitar.Security.Claims;
+
+/// <summary>
+/// Defines methods useful to manipulate claims.
+/// </summary>
+public static class ClaimHelper
+{
+  /// <summary>
+  /// Creates a claim from the specified date and time.
+  /// </summary>
+  /// <param name="name">The name of the claim.</param>
+  /// <param name="moment">The moment value.</param>
+  /// <returns>The created claim.</returns>
+  public static Claim Create(string name, DateTime moment)
+  {
+    string value = new DateTimeOffset(moment.ToUniversalTime()).ToUnixTimeSeconds().ToString();
+
+    return new Claim(name, value, ClaimValueTypes.Integer64);
+  }
+
+  /// <summary>
+  /// Extracts a date and time from the specified claim.
+  /// </summary>
+  /// <param name="claim">The claim to extract from.</param>
+  /// <returns>The extracted date and time.</returns>
+  public static DateTime ExtractDateTime(Claim claim)
+  {
+    long value = long.Parse(claim.Value);
+
+    return DateTime.SpecifyKind(DateTimeOffset.FromUnixTimeSeconds(value).DateTime, DateTimeKind.Utc);
+  }
+}
---
+++ src/Framework/tests/Logitar.Security.UnitTests/Claims/ClaimHelperTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Logitar.Security.Claims;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class ClaimHelperTests
+{
+  [Fact(DisplayName = "It should create the correct claim from a date and time.")]
+  public void It_should_create_the_correct_claim_from_a_date_and_time()
+  {
+    string name = Rfc7519ClaimNames.ExpirationTime;
+    DateTime moment = DateTime.Now;
+    string value = ToUnixTimeSeconds(moment).ToString();
+
+    Claim claim = ClaimHelper.Create(name, moment);
+    Assert.Equal(name, claim.Type);
+    Assert.Equal(value, claim.Value);
+    Assert.Equal(ClaimValueTypes.Integer64, claim.ValueType);
+  }
+
+  [Fact(DisplayName = "It should extract the correct date and time from a claim.")]
+  public void It_should_extract_the_correct_date_and_time_from_a_claim()
+  {
+    DateTime moment = DateTime.Now;
+    string value = ToUnixTimeSeconds(moment).ToString();
+    Claim claim = new(Rfc7519ClaimNames.AuthenticationTime, value, ClaimValueTypes.Integer64);
+
+    DateTime result = ClaimHelper.ExtractDateTime(claim);
+    Assert.Equal(ToUnixTimeSeconds(moment), ToUnixTimeSeconds(result));
+  }
+
+  [Fact(DisplayName = "It should extract the same date and time from the claim it created.")]
+  public void It_should_extract_the_same_date_and_time_from_the_claim_it_created()
+  {
+    DateTime moment = DateTime.Now;
+    Claim claim = ClaimHelper.Create(Rfc7519ClaimNames.UpdatedAt, moment);
+
+    DateTime result = ClaimHelper.ExtractDateTime(claim);
+    Assert.Equal(ToUnixTimeSeconds(moment), ToUnixTimeSeconds(result));
+  }
+
+  private static long ToUnixTimeSeconds(DateTime moment) => new DateTimeOffset(moment).ToUnixTimeSeconds();
+}
