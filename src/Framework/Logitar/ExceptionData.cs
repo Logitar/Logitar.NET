37bbf721b0c11dd1737eb91ca674f6e76217425e
Francis Pion <francispion@hotmail.com>
2023-11-02 01:17:23 -0400

    Implementing ExceptionData.

---
+++ src/Framework/Logitar/ExceptionData.cs
@@ -0,0 +1,65 @@
+﻿namespace Logitar;
+
+/// <summary>
+/// A serialization representation of an <see cref="Exception"/>.
+/// </summary>
+public record ExceptionData
+{
+  /// <summary>
+  /// Gets or sets the type of the current exception.
+  /// </summary>
+  public string Type { get; set; } = string.Empty;
+  /// <summary>
+  /// Gets or sets a message that describes the current exception.
+  /// </summary>
+  public string Message { get; set; } = string.Empty;
+  /// <summary>
+  /// Gets or sets the <see cref="ExceptionData"/> instance that caused the current exception.
+  /// </summary>
+  public ExceptionData? InnerException { get; set; }
+
+  /// <summary>
+  /// Gets or sets HRESULT, a coded numerical value that is assigned to a specific exception.
+  /// </summary>
+  public int HResult { get; set; }
+  /// <summary>
+  /// Gets or sets a link to the help file associated with this exception.
+  /// </summary>
+  public string? HelpLink { get; set; }
+  /// <summary>
+  /// Gets or sets the name of the application or the object that causes the error.
+  /// </summary>
+  public string? Source { get; set; }
+  /// <summary>
+  /// Gets or sets a string representation of the immediate frames on the call stack.
+  /// </summary>
+  public string? StackTrace { get; set; }
+  /// <summary>
+  /// Gets or sets the method that throws the current exception.
+  /// </summary>
+  public string? TargetSite { get; set; }
+
+  // TODO(fpion): Data
+
+  /// <summary>
+  /// Creates a serializable representation of the specified exception.
+  /// </summary>
+  /// <param name="exception">The exception to represent.</param>
+  /// <returns>The serializable representation.</returns>
+  public static ExceptionData From(Exception exception)
+  {
+    // TODO(fpion): Data
+
+    return new ExceptionData
+    {
+      Type = exception.GetType().GetLongestName(),
+      Message = exception.Message,
+      InnerException = exception.InnerException == null ? null : From(exception.InnerException),
+      HResult = exception.HResult,
+      HelpLink = exception.HelpLink,
+      Source = exception.Source,
+      StackTrace = exception.StackTrace,
+      TargetSite = exception.TargetSite?.ToString()
+    };
+  }
+}
---
+++ src/Framework/tests/Logitar.UnitTests/ExceptionDataTests.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using System.Reflection;
+
+namespace Logitar;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class ExceptionDataTests
+{
+  private readonly Faker _faker = new();
+
+  [Fact(DisplayName = "From: it should return the correct exception data.")]
+  public void From_it_should_return_the_correct_exception_data()
+  {
+    Exception innerException = new("INNER_EXCEPTION");
+    Exception exception = new("An unexpected error occurred.", innerException)
+    {
+      HResult = _faker.Random.Number(0, int.MaxValue),
+      HelpLink = $"https://www.{_faker.Internet.DomainName()}",
+      Source = nameof(From_it_should_return_the_correct_exception_data)
+    };
+
+    string stackTrace = nameof(exception.StackTrace);
+    FieldInfo? _stackTraceString = exception.GetType().GetField("_stackTraceString", BindingFlags.NonPublic | BindingFlags.Instance);
+    Assert.NotNull(_stackTraceString);
+    _stackTraceString.SetValue(exception, stackTrace);
+
+    MethodBase? targetSite = MethodBase.GetCurrentMethod();
+    Assert.NotNull(targetSite);
+    FieldInfo? _exceptionMethod = exception.GetType().GetField("_exceptionMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+    Assert.NotNull(_exceptionMethod);
+    _exceptionMethod.SetValue(exception, targetSite);
+
+    // TODO(fpion): Data
+
+    ExceptionData data = ExceptionData.From(exception);
+
+    Assert.Equal(exception.GetType().AssemblyQualifiedName, data.Type);
+    Assert.Equal(exception.Message, data.Message);
+    Assert.Equal(innerException.Message, data.InnerException?.Message);
+    Assert.Equal(exception.HResult, data.HResult);
+    Assert.Equal(exception.HelpLink, data.HelpLink);
+    Assert.Equal(exception.Source, data.Source);
+    Assert.Equal(stackTrace, data.StackTrace);
+    Assert.Equal(exception.TargetSite?.ToString(), data.TargetSite);
+    // TODO(fpion): Data
+  }
+}
