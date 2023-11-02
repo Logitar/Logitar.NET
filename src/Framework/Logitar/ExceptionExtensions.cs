edaf777a1d2a4837f881a55c3b09b57043d0655a
Francis Pion <francispion@hotmail.com>
2023-11-01 21:59:21 -0400

    Added ExceptionExtensions.

---
+++ src/Framework/Logitar/ExceptionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Logitar;
+
+/// <summary>
+/// Provides extension methods for exceptions.
+/// See <see cref="Exception"/> for more information.
+/// </summary>
+public static class ExceptionExtensions
+{
+  /// <summary>
+  /// Formats an error code from the specified exception.
+  /// </summary>
+  /// <param name="exception">The exception to format from.</param>
+  /// <returns>The formatted error code.</returns>
+  public static string GetErrorCode(this Exception exception) => exception.GetType().Name.Remove(nameof(Exception));
+}
