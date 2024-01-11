4142f89e3194793a54290ae815f442fd7af788c1
Francis Pion <francispion@hotmail.com>
2024-01-10 20:31:27 -0500

    Release 1.11 (#173)

--- src/Logitar.Security/Claims/OtherClaimNames.cs
+++ src/Logitar.Security/Claims/OtherClaimNames.cs
@@ -9,9 +9,4 @@ public static class OtherClaimNames
   /// The name of the claim that specifies whether a postal address is verified or not.
   /// </summary>
   public const string IsAddressVerified = "address_verified";
-  /// <summary>
-  /// The name of the claim that specifies a token purpose.
-  /// </summary>
-  [Obsolete("This claim name will be removed in the next major release, since it can be replaced by the token type parameter.")]
-  public const string Purpose = "purpose";
 }
