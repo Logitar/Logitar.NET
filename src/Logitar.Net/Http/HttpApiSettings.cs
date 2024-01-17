287872f572147251d41d4e060f0703761f6c379d
Francis Pion <--global>
2024-01-17 12:18:21 -0500

    Implemented HttpApiSettings.

---
+++ src/Logitar.Net/Http/HttpApiSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace Logitar.Net.Http;
+
+/// <summary>
+/// Defines the settings of an HTTP API client.
+/// </summary>
+public class HttpApiSettings : IHttpApiSettings
+{
+  /// <summary>
+  /// Gets or sets the authentication/authorization settings of the API.
+  /// </summary>
+  public IHttpAuthorization? Authorization { get; set; }
+
+  /// <summary>
+  /// Gets or sets the base Uniform Resource Identifier (URI) of the API.
+  /// </summary>
+  public Uri? BaseUri { get; set; }
+
+  /// <summary>
+  /// Gets or sets the request headers.
+  /// </summary>
+  public List<HttpHeader> Headers { get; set; } = [];
+
+  /// <summary>
+  /// Gets or sets a value indicating whether or not to throw an <see cref="HttpFailureException"/> when a HTTP response does not indicate success.
+  /// </summary>
+  public bool ThrowOnFailure { get; set; } = true;
+}
---
+++ src/Logitar.Net/Http/IHttpApiSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace Logitar.Net.Http;
+
+/// <summary>
+/// Defines the settings of an HTTP API client.
+/// </summary>
+public interface IHttpApiSettings
+{
+  /// <summary>
+  /// Gets the authentication/authorization settings of the API.
+  /// </summary>
+  IHttpAuthorization? Authorization { get; }
+
+  /// <summary>
+  /// Gets the base Uniform Resource Identifier (URI) of the API.
+  /// </summary>
+  Uri? BaseUri { get; }
+
+  /// <summary>
+  /// Gets the request headers.
+  /// </summary>
+  List<HttpHeader> Headers { get; }
+
+  /// <summary>
+  /// Gets a value indicating whether or not to throw an <see cref="HttpFailureException"/> when a HTTP response does not indicate success.
+  /// </summary>
+  bool ThrowOnFailure { get; }
+}
