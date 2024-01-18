e55394880a0e83413f085fb17bd6666e06c28074
Francis Pion <--global>
2024-01-18 11:48:59 -0500

    Defined a mail client.

---
+++ src/Logitar.Net/Mail/IMailClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mail;
+
+namespace Logitar.Net.Mail;
+
+/// <summary>
+/// Defines methods to manage email messages.
+/// </summary>
+public interface IMailClient
+{
+  /// <summary>
+  /// Sends the specified email message.
+  /// </summary>
+  /// <param name="message">The message to send.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The operation result.</returns>
+  Task<SendMailResult> SendAsync(MailMessage message, CancellationToken cancellationToken = default);
+}
