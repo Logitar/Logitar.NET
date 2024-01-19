16578454e93ad91f0a79e396ec502ecbf03c4bb7
Francis Pion <francispion@hotmail.com>
2024-01-19 00:09:11 -0500

    Implemented SendGrid API settings.

---
+++ src/Logitar.Net.Mail.SendGrid/Settings/IEndPointSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Logitar.Net.Mail.SendGrid.Settings;
+
+/// <summary>
+/// Defines the settings of a SendGrid API endpoint.
+/// </summary>
+public interface IEndPointSettings
+{
+  /// <summary>
+  /// Gets string representation of the endpoint's HTTP method.
+  /// </summary>
+  string Method { get; }
+  /// <summary>
+  /// Gets the HTTP method of the endpoint.
+  /// </summary>
+  HttpMethod HttpMethod { get; }
+
+  /// <summary>
+  /// Gets a string representation of the endpoint's path.
+  /// </summary>
+  string Path { get; }
+  /// <summary>
+  /// Gets the path of the endpoint.
+  /// </summary>
+  Uri UriPath { get; }
+}
---
+++ src/Logitar.Net.Mail.SendGrid/Settings/SendGridSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace Logitar.Net.Mail.SendGrid.Settings;
+
+/// <summary>
+/// Implements the settings of the SendGrid API.
+/// </summary>
+public class SendGridSettings : ISendGridSettings
+{
+  /// <summary>
+  /// Gets or sets the API key to authorize the SendGrid API calls.
+  /// </summary>
+  public string? ApiKey { get; set; }
+
+  /// <summary>
+  /// Gets or sets the base Uniform Resource Locator (URL) of the SendGrid API.
+  /// </summary>
+  public string BaseUrl { get; set; } = "https://api.sendgrid.com";
+  /// <summary>
+  /// Gets the base Uniform Resource Identifier (URI) of the SendGrid API.
+  /// </summary>
+  public Uri BaseUri => new(BaseUrl, UriKind.Absolute);
+
+  /// <summary>
+  /// Gets or sets the send mail end point information.
+  /// </summary>
+  public EndPointSettings SendMail { get; set; } = new()
+  {
+    Method = "POST",
+    Path = "/v3/mail/send"
+  };
+}
