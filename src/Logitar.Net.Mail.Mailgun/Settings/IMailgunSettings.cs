632f6ebc3ac7c698acefe84f27c1a0419b9d1708
Francis Pion <francispion@hotmail.com>
2024-02-25 01:04:35 -0500

    Implementing a MailgunClient.

---
+++ src/Logitar.Net.Mail.Mailgun/Settings/IMailgunSettings.cs
@@ -0,0 +1,39 @@
+﻿namespace Logitar.Net.Mail.Mailgun.Settings;
+
+/// <summary>
+/// Defines the settings of the Mailgun API.
+/// </summary>
+public interface IMailgunSettings
+{
+  /// <summary>
+  /// Gets the username used to authorize the Mailgun API calls.
+  /// </summary>
+  string Username { get; }
+  /// <summary>
+  /// Gets the password used to authorize the Mailgun API calls, typically your API key.
+  /// </summary>
+  string? Password { get; }
+  /// <summary>
+  /// Gets the API key used to authorize the Mailgun API calls.
+  /// </summary>
+  string? ApiKey { get; }
+
+  /// <summary>
+  /// Gets the base Uniform Resource Locator (URL) of the Mailgun API.
+  /// </summary>
+  string BaseUrl { get; }
+  /// <summary>
+  /// Gets the base Uniform Resource Identifier (URI) of the Mailgun API.
+  /// </summary>
+  Uri BaseUri { get; }
+
+  /// <summary>
+  /// Gets the name of the domain to send messages from.
+  /// </summary>
+  string? DomainName { get; }
+
+  /// <summary>
+  /// Gets the send mail end point information.
+  /// </summary>
+  EndPointSettings SendMail { get; }
+}
---
+++ src/Logitar.Net.Mail.Mailgun/Settings/MailgunSettingsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Logitar.Net.Http;
+
+namespace Logitar.Net.Mail.Mailgun.Settings;
+
+/// <summary>
+/// Defines extension methods for the Mailgun API settings.
+/// </summary>
+public static class MailgunSettingsExtensions
+{
+  /// <summary>
+  /// Returns an instance of HTTP API settings from the specified Mailgun API settings.
+  /// </summary>
+  /// <param name="settings">The Mailgun API settings.</param>
+  /// <returns>The HTTP API settings.</returns>
+  public static IHttpApiSettings ToHttpApiSettings(this IMailgunSettings settings)
+  {
+    HttpApiSettings apiSettings = new()
+    {
+      BaseUri = settings.BaseUri
+    };
+
+    if (!string.IsNullOrWhiteSpace(settings.Password))
+    {
+      apiSettings.Authorization = HttpAuthorization.Basic(settings.Username.Trim(), settings.Password.Trim());
+    }
+
+    return apiSettings;
+  }
+}
